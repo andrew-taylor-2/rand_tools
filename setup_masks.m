3f621f2efe23597b89a20b0cbf3d52c926691765
James Andrew Taylor <taylora5@g.cofc.edu>
2021-02-25 12:40:34 -0500

    fixes cmd addition, allowing for use in simple_regression

--- setup_masks.m
+++ setup_masks.m
@@ -9,9 +9,9 @@ outcon=[outbasename '.con'];
 [~,outmsg]=system(['setup_masks ' inmat ' ' incon ' ' outbasename ' ' fnpattern_3dvols])
 
 % get a randomise command addendum (the vxf and vxl inputs)
-ind=strfind(outmsg,'--vxf=');
-vxf_addendum=strrep(outmsg(ind:ind+7),' ','') %this will fail if mask dim 4 is 100 or more entries; this is a hitherto unencountered case so if you come across it think of something a little more clever for this section, big lol 
+ind=strfind(outmsg,'--vxl=');
+vxl_addendum=strrep(outmsg(ind:ind+7),' ',''); %this will fail if mask dim 4 is 100 or more entries; this is a hitherto unencountered case so if you come across it think of something a little more clever for this section, big lol 
 
-randomise_cmd_addendum=[' --vxf=' outbasename ' ' vxf_addendum];
+randomise_cmd_addendum=[' --vxf=' outbasename ' ' vxl_addendum];
 
 end
